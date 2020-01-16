1cfc20d49f564d06cd3315335b388fb3b7875b34
Daniele Lanzi <dnlshiva@gmail.com>
2020-01-16 23:42:05 +0100

    correzione palindroma 2 versioni

--- js-palindroma/js/script.js
+++ js-palindroma/js/script.js
@@ -1,17 +1,14 @@
 // <===========================INIZIO========================>
 // Scrivi una funzione per capire se la parola è palindroma
 
-var palindroma = [''];
+
 do {
   var parola = prompt('Verifica se la parola è un palindroma');
 } while (parola.length == 0 );
 
-for (var i = parola.length -1; i >= 0; i--) {
-  palindroma[0] += parola[i];
-  console.log(palindroma[0] += parola[i]);
-}
+var parolaTurn = wordReverse(parola);
 
-if (palindroma[0] == parola) {
+if (parolaTurn == parola) {
   console.log('Hai inserito una parola palindroma: ' + parola);
   alert('Hai inserito una parola palindroma');
 } else {
@@ -20,3 +17,23 @@ if (palindroma[0] == parola) {
 }
 
 // <===========================FINE========================>
+
+var parola = prompt('Verifica se la parola è un palindroma');
+var parolaTurn = wordReverse(parola);
+
+if (parolaTurn == parola) {
+  console.log('Hai inserito una parola palindroma: ' + parola);
+  alert('Hai inserito una parola palindroma');
+} else {
+  console.log('Non hai inserito una parola palindroma: ' + parola);
+  alert('Non hai inserito una parola palindroma');
+};
+
+// Funzioni
+function wordReverse(parola) {
+  var parolaReverse = '';
+  for (var i = parola.length -1; i >= 0; i--) {
+    parolaReverse += parola[i];
+  }
+  return parolaReverse
+};
