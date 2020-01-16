d66a03a7a99359ecc5275e76685dfeb572fff754
Daniele Lanzi <dnlshiva@gmail.com>
2020-01-16 21:46:04 +0100

    perfezione esercizio pari e dispari

--- js-pari-dispari/js/script.js
+++ js-pari-dispari/js/script.js
@@ -6,37 +6,58 @@
 // Sommiamo i due numeri e dichiariamo chi ha
 // vinto (controlliamo se la somma da pari o dispari).
 
-if (confirm('Pronto per giocare?')) {
-  do {
-    var sceltaUtente = prompt('Scegli tra \'pari\' o \'dispari\'').toLowerCase();
-  } while (sceltaUtente != 'pari' && sceltaUtente != 'dispari');
-}else {
-  alert('Hai annullato il gioco, ricarica la pagina per giocare di nuovo');
-};
-do {
-  var numeroUtente = parseInt(prompt('Scegli un numero da 1 a 5'));
-} while (
-  isNaN(numeroUtente)
-);
-
 var numeroPc = parseInt(getRandomNumber(0,5));
 console.log('Numero casuale da 1 a 5 generato dal pc: ' + numeroPc);
 
-if ((numeroUtente + numeroPc) % 2 == 0 && sceltaUtente == 'pari') {
-  console.log('Somma dei numeri: ' + (numeroUtente + numeroPc));
-  console.log('Hai Vinto!');
-  alert('Hai Vinto!')
-} else if ((numeroUtente + numeroPc) % 2 != 0 && sceltaUtente == 'dispari') {
-  console.log('Somma dei numeri: ' + (numeroUtente + numeroPc));
-  console.log('Hai Vinto!');
-  alert('Hai Vinto!')
+if (confirm('Pronto per giocare?')) {
+  do {
+    // Ciclo scelta pari e dispari
+    do {
+      var sceltaUtente = prompt('Scegli tra \'pari\' o \'dispari\'').toLowerCase();
+      if (sceltaUtente != 'pari' && sceltaUtente != 'dispari') {
+        alert('Devi inserire pari o dispari')
+      }
+    } while (
+      (sceltaUtente != 'pari' && sceltaUtente != 'dispari')
+    );
+
+    // Ciclo scelta numero da 1 a 5
+    do {
+      var numeroUtente = parseInt(prompt('Scegli un numero da 1 a 5'));
+      if (numeroUtente > 5 || numeroUtente < 1 || isNaN(numeroUtente))  {
+        alert('Devi inserire un numero da 1 a 5')
+      }
+    } while (numeroUtente > 5 || numeroUtente < 1 || isNaN(numeroUtente));
+  } while (
+    (sceltaUtente != 'pari' && sceltaUtente != 'dispari') ||
+    isNaN(numeroUtente)
+  );
+
+  // Ciclo numero random e risultato
+  do {
+    if ((numeroUtente + numeroPc) % 2 == 0 && sceltaUtente == 'pari') {
+      console.log('Somma dei numeri: ' + (numeroUtente + numeroPc));
+      console.log('Hai Vinto!');
+      alert('Hai Vinto!')
+    } else if ((numeroUtente + numeroPc) % 2 != 0 && sceltaUtente == 'dispari') {
+      console.log('Somma dei numeri: ' + (numeroUtente + numeroPc));
+      console.log('Hai Vinto!');
+      alert('Hai Vinto!')
+    } else {
+      console.log('Somma dei numeri: ' + (numeroUtente + numeroPc));
+      console.log('Hai Perso!');
+      alert('Hai Perso!')
+    };
+  } while ((sceltaUtente != 'pari' && sceltaUtente != 'dispari') ||
+  isNaN(numeroUtente));
+
 } else {
-  console.log('Somma dei numeri: ' + (numeroUtente + numeroPc));
-  console.log('Hai Perso!');
-  alert('Hai Perso!')
-}
+  alert('Hai annullato il gioco, ricarica la pagina per giocare di nuovo');
+};
+
 
 // Funzioni
+// Numero random
 function getRandomNumber(min,max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -45,6 +66,7 @@ function getRandomNumber(min,max) {
   return random;
 };
 
+// Ricarica pagina
 function ricaricaPage() {
   location.replace('file:///Users/danielelanzi/Documents/boolean-courses/consegna-esercizi/js-paliedispari/js-pari-dispari/index.html');
 };
